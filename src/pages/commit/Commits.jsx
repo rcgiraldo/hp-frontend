eda22fbf7374f6ef1ea3be971d57b915c7fce955
Ruben Giraldo <ru.giraldo@gmail.com>
2023-04-30 14:15:31 -0500

    Added commits page

---
+++ src/pages/commit/Commits.jsx
@@ -0,0 +1,35 @@
+import { useEffect } from "react";
+import { getCommits } from "../../services/commitService";
+
+const Commits = () => {
+  const [commitList, setCommitList] = useState([]);
+
+  useEffect(() => {
+    let list = getCommits();
+
+    list.then((res) => {
+      console.log("list of commits");
+      console.log(res);
+      setCommitList(res);
+    });
+  }, []);
+
+  return (
+    <>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 8,
+        }}
+      >
+		<Container maxWidth={false}>
+			<Box sx={{ mt: 3}} >
+				<CommitList commitList={commitList} />
+			</Box>
+
+		</Container>
+	  </Box>
+    </>
+  );
+};
